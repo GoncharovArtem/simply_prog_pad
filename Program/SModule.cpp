4eff65468c31ad52956dd0e5b0ac95c4ef43351f
GoncharovArtem <artemvalerevich2704@gmail.com>
2024-05-06 03:06:08 +0300

    Произведено добавление комментариев базовых методов и полей класса SModule

--- Program/SModule.cpp
+++ Program/SModule.cpp
@@ -2,10 +2,15 @@
 
 class SModule
 {
+    // Номер загружаемого модуля
     size_t C;
+    // Номера загруженных модулей
     vector<int> data;
 public:
+    // Конструктор класса
     M(std::size_t R, std::size_t C) : C(C), data(R* C) {}
+    // Процедура поиска модуля перед загрузкой
     int z_operator()(std::size_t r, std::size_t c) const;
+    // Дружественная функция поиска модуля перед загрузкой
     int& z_operator()(std::size_t r, std::size_t c);
 };
