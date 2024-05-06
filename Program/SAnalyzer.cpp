996ca238098072ba133fd364f01340eb9d80943d
GoncharovArtem <artemvalerevich2704@gmail.com>
2024-05-06 03:20:26 +0300

    Произведено добавление комментариев к полям и базовым методам класса SAnalyzer

--- Program/SAnalyzer.cpp
+++ Program/SAnalyzer.cpp
@@ -3,10 +3,15 @@
 class SAnalyzer
 {
 private:
+    // Строка подвергающаяся проверке семантики
     string incoming_to_analyze;
+    // Проверяемая фраза
     string checking_str;
 public: 
+    // Реализация отклика для пользователя о результатах проверки
     void analyzeMessage();
+    // Установка проверяемой фразы из входящей строки
     void setChecking(string inc_s);
+    // Получение проверяемой фразы
     string getChecking();
 };
